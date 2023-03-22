dbce41bafdee05ddef28bb1ae88797aa1c7b754c
GustavoMota0 <125700102+GustavoMota0@users.noreply.github.com>
2023-03-21 22:11:48 -0300

    Add files via upload

---
+++ Atividade4/Atividade4/ClassificacaoTriangulo.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace ClassificacaoTriangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtLado1 = new System.Windows.Forms.TextBox();
+            this.txtLado2 = new System.Windows.Forms.TextBox();
+            this.txtLado3 = new System.Windows.Forms.TextBox();
+            this.lblLado1 = new System.Windows.Forms.Label();
+            this.lblLado2 = new System.Windows.Forms.Label();
+            this.lblLado3 = new System.Windows.Forms.Label();
+            this.txtTriangulo = new System.Windows.Forms.TextBox();
+            this.btnClassificar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLado1
+            // 
+            this.txtLado1.Location = new System.Drawing.Point(502, 175);
+            this.txtLado1.Name = "txtLado1";
+            this.txtLado1.Size = new System.Drawing.Size(167, 26);
+            this.txtLado1.TabIndex = 0;
+            // 
+            // txtLado2
+            // 
+            this.txtLado2.Location = new System.Drawing.Point(502, 246);
+            this.txtLado2.Name = "txtLado2";
+            this.txtLado2.Size = new System.Drawing.Size(167, 26);
+            this.txtLado2.TabIndex = 1;
+            // 
+            // txtLado3
+            // 
+            this.txtLado3.Location = new System.Drawing.Point(502, 319);
+            this.txtLado3.Name = "txtLado3";
+            this.txtLado3.Size = new System.Drawing.Size(167, 26);
+            this.txtLado3.TabIndex = 2;
+            // 
+            // lblLado1
+            // 
+            this.lblLado1.AutoSize = true;
+            this.lblLado1.Location = new System.Drawing.Point(438, 178);
+            this.lblLado1.Name = "lblLado1";
+            this.lblLado1.Size = new System.Drawing.Size(58, 20);
+            this.lblLado1.TabIndex = 3;
+            this.lblLado1.Text = "Lado 1";
+            // 
+            // lblLado2
+            // 
+            this.lblLado2.AutoSize = true;
+            this.lblLado2.Location = new System.Drawing.Point(438, 249);
+            this.lblLado2.Name = "lblLado2";
+            this.lblLado2.Size = new System.Drawing.Size(58, 20);
+            this.lblLado2.TabIndex = 4;
+            this.lblLado2.Text = "Lado 2";
+            // 
+            // lblLado3
+            // 
+            this.lblLado3.AutoSize = true;
+            this.lblLado3.Location = new System.Drawing.Point(438, 322);
+            this.lblLado3.Name = "lblLado3";
+            this.lblLado3.Size = new System.Drawing.Size(58, 20);
+            this.lblLado3.TabIndex = 5;
+            this.lblLado3.Text = "Lado 3";
+            // 
+            // txtTriangulo
+            // 
+            this.txtTriangulo.Location = new System.Drawing.Point(442, 387);
+            this.txtTriangulo.Name = "txtTriangulo";
+            this.txtTriangulo.ReadOnly = true;
+            this.txtTriangulo.Size = new System.Drawing.Size(227, 26);
+            this.txtTriangulo.TabIndex = 6;
+            // 
+            // btnClassificar
+            // 
+            this.btnClassificar.Location = new System.Drawing.Point(442, 450);
+            this.btnClassificar.Name = "btnClassificar";
+            this.btnClassificar.Size = new System.Drawing.Size(99, 37);
+            this.btnClassificar.TabIndex = 8;
+            this.btnClassificar.Text = "Classificar";
+            this.btnClassificar.UseVisualStyleBackColor = true;
+            this.btnClassificar.Click += new System.EventHandler(this.btnClassificar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(570, 450);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(99, 38);
+            this.btnFechar.TabIndex = 9;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnClassificar);
+            this.Controls.Add(this.txtTriangulo);
+            this.Controls.Add(this.lblLado3);
+            this.Controls.Add(this.lblLado2);
+            this.Controls.Add(this.lblLado1);
+            this.Controls.Add(this.txtLado3);
+            this.Controls.Add(this.txtLado2);
+            this.Controls.Add(this.txtLado1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLado1;
+        private System.Windows.Forms.TextBox txtLado2;
+        private System.Windows.Forms.TextBox txtLado3;
+        private System.Windows.Forms.Label lblLado1;
+        private System.Windows.Forms.Label lblLado2;
+        private System.Windows.Forms.Label lblLado3;
+        private System.Windows.Forms.TextBox txtTriangulo;
+        private System.Windows.Forms.Button btnClassificar;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
